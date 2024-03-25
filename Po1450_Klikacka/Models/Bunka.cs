3f166e312be7c24c14af34db47f16e75c56f4e9e
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-03-25 16:12:12 +0100

    Přidejte soubory projektu.

---
+++ Po1450_Klikacka/Models/Bunka.cs
@@ -0,0 +1,17 @@
+﻿namespace Po1450_Klikacka.Models
+{
+    public class Bunka
+    {
+        public Bunka(int radka, int sloupec, int hod) 
+        { 
+            Radka = radka;
+            Sloupec = sloupec;
+            Hodnota = hod;
+        }
+        public int Radka { get; private set; }
+        public int Sloupec { get; private set; }
+        public int Hodnota { get; private set; }
+        public int StyleIndexBarvy { get; set; } = 0;
+        public string StyleClassName => $"barva-{StyleIndexBarvy}";
+    }
+}
---
+++ Po1450_Klikacka/Models/Grafika.cs
@@ -0,0 +1,34 @@
+﻿namespace Po1450_Klikacka.Models
+{
+    public class Grafika
+    {
+        public Grafika() 
+        {
+            NahodneMatici(6, 9, 0, 1);
+        }
+        public Bunka[,] Matice { get; private set; }
+        private int ZvolenaBarva { get; set; } = 2;
+
+        private Random rnd = new Random();
+        private void NahodneMatici(int radku, int sloupcu, int minCislo, int maxCislo )
+        {
+            Matice = new Bunka[radku, sloupcu];
+
+            for ( int i = 0; i < radku; i++ )
+            {
+                for (int j = 0; j < sloupcu; j++)
+                {
+                    Matice[i, j] = new Bunka(i, j, rnd.Next(minCislo, maxCislo + 1));
+                }
+            }
+        }
+
+        public void OnCellClick(Models.Bunka bunka)
+        {
+            if (bunka.Hodnota == 0)
+            {
+                bunka.StyleIndexBarvy = ZvolenaBarva;
+            }
+        }
+    }
+}
